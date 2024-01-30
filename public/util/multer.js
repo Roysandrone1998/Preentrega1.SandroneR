f049a2ac8a619265d1ca6f5574e8ce4f3a0aa1fa
Roysandrone1998 <raysandrone@gmail.com>
2024-01-30 20:57:24 -0300

    Terminado

--- public/util/multer.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-app.use("/static", express.static("public"));
-
-app.listen(8080,()=> console.log("Server listening on port 8080"));
